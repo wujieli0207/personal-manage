d61d4d98015e21e6816a266cab106f8add69069e
wujieli0207 <wujieli0207@outlook.com>
2022-05-14 20:37:52 +0800

    fix: 修复ts提示异常问题

--- types/global.d.ts
+++ types/global.d.ts
@@ -1,16 +1,17 @@
 import type { PropType as VuePropType } from "vue";
 
 declare global {
-  declare type Recordable<T = any> = Record<string, T>;
+  type Recordable<T = any> = Record<string, T>;
 
-  declare type Nullable<T> = T | null;
+  type Nullable<T> = T | null;
 
-  declare type DeepPartial<T> = {
+  type DeepPartial<T> = {
     [P in keyof T]?: DeepPartial<T[P]>;
   };
 
-  declare interface ViteEnv {
+  interface ViteEnv {
     VITE_PORT: number; // 项目端口号
     VITE_USE_MOCK: boolean; // 是否使用 mock 数据
+    VITE_PUBLIC_PATH: string;
   }
 }
