cad00680eb0b96c59e511fc23d2c0ed308834e57
wujieli0207 <wujieli0207@outlook.com>
2022-05-15 11:18:09 +0800

    feat: 增加弹出框组件封装

---
+++ src/hooks/web/useMessage.ts
@@ -0,0 +1,65 @@
+import { ElNotification, ElMessage, ElMessageBox } from "element-plus";
+
+interface alertOptions {
+  message: string;
+  title?: string;
+}
+
+/**
+ *
+ * @description success消息提示
+ */
+function createSuccessAlert(options: alertOptions) {
+  const { message, title = "提示信息" } = options;
+  return ElMessageBox.alert(message, title, {
+    type: "success",
+    confirmButtonText: "确定",
+  });
+}
+
+/**
+ *
+ * @description info消息提示
+ */
+function createInfoAlert(options: alertOptions) {
+  const { message, title = "提示信息" } = options;
+  return ElMessageBox.alert(message, title, {
+    type: "info",
+    confirmButtonText: "确定",
+  });
+}
+
+/**
+ *
+ * @description warn消息提示
+ */
+function createWarnAlert(options: alertOptions) {
+  const { message, title = "提示信息" } = options;
+  return ElMessageBox.alert(message, title, {
+    type: "warning",
+    confirmButtonText: "确定",
+  });
+}
+
+/**
+ *
+ * @description error消息提示
+ */
+function createErrorAlert(options: alertOptions) {
+  const { message, title = "提示信息" } = options;
+  return ElMessageBox.alert(message, title, {
+    type: "error",
+    confirmButtonText: "确定",
+  });
+}
+
+export function useMessage() {
+  return {
+    notification: ElNotification,
+    createMessage: ElMessage,
+    createSuccessAlert,
+    createInfoAlert,
+    createWarnAlert,
+    createErrorAlert,
+  };
+}
