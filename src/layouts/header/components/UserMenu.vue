fe4c2777394cf9445b7ff7aab78d3142ab352246
wujieli0207 <wujieli0207@outlook.com>
2022-05-22 11:32:57 +0800

    style: 用户头像菜单抽取为一个文件

---
+++ src/layouts/header/components/UserMenu.vue
@@ -0,0 +1,43 @@
+<template>
+  <el-dropdown
+    class="flex items-center h-12 p-4 text-center cursor-pointer hover:bg-slate-700"
+  >
+    <div class="flex items-center">
+      <el-avatar :size="30" :src="avatar" />
+      <span class="ml-2 text-sm text-white">wujieli</span>
+    </div>
+    <template #dropdown>
+      <el-dropdown-menu>
+        <el-dropdown-item>
+          <a href="javascript:;">帮助文档</a>
+        </el-dropdown-item>
+        <el-dropdown-item>
+          <a href="javascript:;">个人中心</a>
+        </el-dropdown-item>
+        <el-dropdown-item divided>
+          <a @click="handleLogout">退出登录</a>
+        </el-dropdown-item>
+      </el-dropdown-menu>
+    </template>
+  </el-dropdown>
+</template>
+
+<script lang="ts" setup>
+// TODO 头像链接暂时写死
+import avatar from "/@/assets/image/avatar.jpg";
+import { useMessage } from "/@/hooks/web/useMessage";
+import { useUserStore } from "/@/store/modules/user";
+
+const userStore = useUserStore();
+
+const { createConfirmMessage } = useMessage();
+
+/**
+ * @description 退出登录
+ */
+function handleLogout() {
+  createConfirmMessage({ message: "确认是否要退出" }, () => {
+    return userStore.logout(true);
+  });
+}
+</script>
