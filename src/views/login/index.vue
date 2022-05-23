ab00fc8fb4700f266d2c25c15267906be7ae941a
wujieli0207 <wujieli0207@outlook.com>
2022-05-23 20:44:33 +0800

    style: 根据 stylelint 统一代码风格

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -87,68 +87,37 @@ async function handleLogin() {
 
 <style scoped lang="less">
 @import "/@/styles/color.less";
-
 @property --perA {
   syntax: "<percentage>";
   inherits: false;
   initial-value: 75%;
 }
-
 @property --perB {
   syntax: "<percentage>";
   inherits: false;
   initial-value: 99%;
 }
-
 @property --perC {
   syntax: "<percentage>";
   inherits: false;
   initial-value: 15%;
 }
-
 @property --perD {
   syntax: "<percentage>";
   inherits: false;
   initial-value: 16%;
 }
-
 @property --perE {
   syntax: "<percentage>";
   inherits: false;
   initial-value: 86%;
 }
-
 @property --angle {
   syntax: "<angle>";
   inherits: false;
   initial-value: 0deg;
 }
-
 .login-page {
-  background-image: radial-gradient(
-      circle at var(--perE) 7%,
-      rgba(40, 40, 40, 0.04) 0%,
-      rgba(40, 40, 40, 0.04) 50%,
-      rgba(200, 200, 200, 0.04) 50%,
-      rgba(200, 200, 200, 0.04) 100%
-    ),
-    radial-gradient(
-      circle at var(--perC) var(--perD),
-      rgba(99, 99, 99, 0.04) 0%,
-      rgba(99, 99, 99, 0.04) 50%,
-      rgba(45, 45, 45, 0.04) 50%,
-      rgba(45, 45, 45, 0.04) 100%
-    ),
-    radial-gradient(
-      circle at var(--perA) var(--perB),
-      rgba(243, 243, 243, 0.04) 0%,
-      rgba(243, 243, 243, 0.04) 50%,
-      rgba(37, 37, 37, 0.04) 50%,
-      rgba(37, 37, 37, 0.04) 100%
-    ),
-    linear-gradient(var(--angle), #22deed, #8759d7);
-  animation: move 30s infinite alternate linear;
-
   @keyframes move {
     100% {
       --perA: 85%;
@@ -159,25 +128,28 @@ async function handleLogin() {
       --angle: 360deg;
     }
   }
-
-  .login-page__wrapper {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    padding: 40px 50px;
-    background-color: rgba(@color-white, 0.2);
-    border-radius: 15px;
-
-    .login-page__title {
-      padding-bottom: 10px;
-      font-size: 1.4rem;
-      color: @color-white;
-    }
-
-    .login-page__item {
-      width: 240px;
-      margin-top: 5px;
-    }
-  }
+  background-image: radial-gradient(
+      circle at var(--perE) 7%,
+      rgb(40 40 40 / 0.04) 0%,
+      rgb(40 40 40 / 0.04) 50%,
+      rgb(200 200 200 / 0.04) 50%,
+      rgb(200 200 200 / 0.04) 100%
+    ),
+    radial-gradient(
+      circle at var(--perC) var(--perD),
+      rgb(99 99 99 / 0.04) 0%,
+      rgb(99 99 99 / 0.04) 50%,
+      rgb(45 45 45 / 0.04) 50%,
+      rgb(45 45 45 / 0.04) 100%
+    ),
+    radial-gradient(
+      circle at var(--perA) var(--perB),
+      rgb(243 243 243 / 0.04) 0%,
+      rgb(243 243 243 / 0.04) 50%,
+      rgb(37 37 37 / 0.04) 50%,
+      rgb(37 37 37 / 0.04) 100%
+    ),
+    linear-gradient(var(--angle), #22deed, #8759d7);
+  animation: move 30s infinite alternate linear;
 }
 </style>
