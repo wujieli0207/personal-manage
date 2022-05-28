6ef36076ce73d3beb64f5407f0e1fcd9bf0856f9
wujieli0207 <wujieli0207@outlook.com>
2022-05-28 13:56:04 +0800

    build(cli): 引入代码提交校验工具

---
+++ commitlint.config.js
@@ -0,0 +1,39 @@
+module.exports = {
+  ignores: [(commit) => commit.includes("init")],
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    // body 以空白行开头
+    "body-leading-blank": [2, "always"],
+    // footer 以空白行开头
+    "footer-leading-blank": [1, "always"],
+    // header 最大长度
+    "header-max-length": [2, "always", 108],
+    // Header 的 type 不能为空
+    "type-empty": [2, "never"],
+    // Header 的 subject 不能为空
+    "subject-empty": [2, "never"],
+    // 不限制 subject 的写法（['sentence-case', 'start-case', 'pascal-case', 'upper-case']）
+    "subject-case": [0],
+    "type-enum": [
+      2,
+      "always",
+      [
+        "feat",
+        "fix",
+        "perf",
+        "style",
+        "docs",
+        "test",
+        "refactor",
+        "build",
+        "ci",
+        "chore",
+        "revert",
+        "wip",
+        "workflow",
+        "types",
+        "release",
+      ],
+    ],
+  },
+};
