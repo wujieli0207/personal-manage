4f6a914563c391671c64f3397c704f944a45f6ab
wujieli0207 <wujieli0207@outlook.com>
2022-09-18 19:05:42 +0800

    refactor: prettier 统一格式化所有代码

--- src/components/Form/src/BasicForm.vue
+++ src/components/Form/src/BasicForm.vue
@@ -13,37 +13,37 @@
 </template>
 
 <script lang="ts" setup>
-  import { cloneDeep } from "lodash-es";
-  import { computed, onMounted, reactive, ref, unref, useAttrs } from "vue";
-  import { basicProps } from "./props";
-  import FormItem from "./components/FormItem.vue";
-  import { FormSchema, PersonFormProps } from "./types/form";
-  import { useFormValues } from "./hooks/useFormValues";
+  import { cloneDeep } from 'lodash-es'
+  import { computed, onMounted, reactive, ref, unref, useAttrs } from 'vue'
+  import { basicProps } from './props'
+  import FormItem from './components/FormItem.vue'
+  import { FormSchema, PersonFormProps } from './types/form'
+  import { useFormValues } from './hooks/useFormValues'
 
-  const props = defineProps(basicProps);
-  const attrs = useAttrs();
+  const props = defineProps(basicProps)
+  const attrs = useAttrs()
 
-  const formModel = reactive<Recordable>({});
+  const formModel = reactive<Recordable>({})
 
-  const defaultValueRef = ref<Recordable>({});
+  const defaultValueRef = ref<Recordable>({})
 
   const getProps = computed((): PersonFormProps => {
-    return { ...props } as PersonFormProps;
-  });
+    return { ...props } as PersonFormProps
+  })
 
   const getSchema = computed((): FormSchema[] => {
-    const schemas = props.schemas;
+    const schemas = props.schemas
 
-    return cloneDeep(schemas) as FormSchema[];
-  });
+    return cloneDeep(schemas) as FormSchema[]
+  })
 
   const getBindValue = computed(() => {
-    return { ...attrs, ...props, ...unref(getProps) };
-  });
+    return { ...attrs, ...props, ...unref(getProps) }
+  })
 
-  const { initDefault } = useFormValues({ defaultValueRef, getSchema, getProps, formModel });
+  const { initDefault } = useFormValues({ defaultValueRef, getSchema, getProps, formModel })
 
   onMounted(() => {
-    initDefault();
-  });
+    initDefault()
+  })
 </script>
